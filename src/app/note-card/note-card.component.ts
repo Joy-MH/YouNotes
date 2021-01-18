3c3e78f9a99873c92be7662f9b821cbe58d0a31b
Joy-MH <mhjoy1998@gmail.com>
2021-01-18 22:03:49 +0600

    Feat: Added Note Card CSS

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() title: string;
+  @Input() body: string;
+
+  @ViewChild('truncator', {static: true}) truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText',  {static: true}) bodyText: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewAbleHeight = parseInt(style.getPropertyValue("height"), 10);
+
+    if(this.bodyText.nativeElement.scrollHeight > viewAbleHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+        this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+
   }
 
 }
