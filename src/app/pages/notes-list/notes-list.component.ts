7c21b6d9ecaf8db80051ab5f8c08bf3c618e1360
Joy-MH <mhjoy1998@gmail.com>
2021-01-22 00:44:24 +0600

    Feat: Added Entry Animation 😵

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      transition('void => *', [
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+        }),
+        animate('50ms', style({
+          height: 0,
+          'margin-bottom': '*',
+          paddingBottom: '*',
+          paddingTop: '*',
+          paddingLeft: '*',
+          paddingRight: '*',
+        })),
+        animate(68)
+      ])
+    ])
+  ]
 })
 export class NotesListComponent implements OnInit {
 
