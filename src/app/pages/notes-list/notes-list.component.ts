4f4a3b0bcefffe1abebd7979e820ce4b27d4aa72
Joy-MH <mhjoy1998@gmail.com>
2021-01-22 16:39:25 +0600

    Feat: Added Delete Animation

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -9,6 +9,7 @@ import {animate, style, transition, trigger} from '@angular/animations';
   styleUrls: ['./notes-list.component.scss'],
   animations: [
     trigger('itemAnim', [
+      // Add animation
       transition('void => *', [
         style({
           height: 0,
@@ -30,6 +31,28 @@ import {animate, style, transition, trigger} from '@angular/animations';
           paddingRight: '*',
         })),
         animate(68)
+      ]),
+      // Delete Animation
+      transition('* => void', [
+        animate(50, style({
+          transform: 'scale(0.85)'
+        })),
+        animate(50, style({
+          transform: 'scale(1)',
+          opacity: 0.75
+        })),
+        animate('120ms ease-out', style({
+          transform: 'scale(0.68)',
+          opacity: 0
+        })),
+        animate('150ms ease-out', style({
+          height: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          'margin-top': '0',
+        }))
       ])
     ])
   ]
